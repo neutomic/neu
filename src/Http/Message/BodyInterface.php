89f42efd1ecd0338201c3972241aa70e1355208a
azjezz <azjezz@protonmail.com>
2022-01-05 21:30:45 +0100

    chore(http): fix naming
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/Http/Message/BodyInterface.php
+++ src/Http/Message/BodyInterface.php
@@ -12,14 +12,14 @@ interface BodyInterface
      * This method must be implemented in such as way that it wait for previous read
      * operations to finish before attempting to read from the body.
      *
-     * Up to `$max_bytes` may be allocated in a buffer; large values may lead to
-     * unnecessarily hitting the request memory limit.s
+     * Up to `$length` may be allocated in a buffer; large values may lead to
+     * unnecessarily hitting the request memory limit.
      *
-     * @param ?positive-int $max_bytes the maximum number of bytes to read
+     * @param ?positive-int $length the maximum number of bytes to read
      *
      * @return string the read data on success, or an empty string if the end of file is reached, or the body has been closed.
      */
-    public function read(?int $max_bytes = null): string;
+    public function read(?int $length = null): string;
 
     /**
      * Read until there is no more data to read.
@@ -27,12 +27,12 @@ interface BodyInterface
      * This method must be implemented in such as way that it wait for previous read
      * operations to finish before attempting to read from the body.
      *
-     * Up to `$max_bytes` may be allocated in a buffer; large values may lead to
+     * Up to `$length` may be allocated in a buffer; large values may lead to
      * unnecessarily hitting the request memory limit.
      *
-     * @param ?positive-int $max_bytes the maximum number of bytes to read
+     * @param ?positive-int $length the maximum number of bytes to read
      *
      * @return string the read data on success, or an empty string if the end of file is reached, or the body has been closed.
      */
-    public function readAll(?int $max_bytes = null): string;
+    public function readAll(?int $length = null): string;
 }
